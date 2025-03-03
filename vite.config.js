db335f08dbbe0c7ce4e7ab718c9957f7badd64c8
yuling <gb2013429@gmail.com>
2025-03-03 23:37:33 +0800

    git init-3

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import Components from 'unplugin-vue-components/vite';
 import {PrimeVueResolver} from '@primevue/auto-import-resolver';
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/steptogether/',
   plugins: [
     vue(),
     Components({
