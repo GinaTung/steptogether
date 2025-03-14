3985af844c9cdb83b076d5f842ee6eb7afe8f023
yuling <gb2013429@gmail.com>
2025-03-14 23:24:02 +0800

    fix:install

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-import tailwindcssPrimeui from "tailwindcss-primeui";
-
-export default {
-  content: [
-    "./public/**/*.html",
-    "./src/**/*.{js,jsx,ts,tsx,vue}",
-  ],
-  plugins: [tailwindcssPrimeui], // 如果你有使用 tailwindcssPrimeui
-};
