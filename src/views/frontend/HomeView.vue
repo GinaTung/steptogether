05974e3f0bcef05dae5e38cf29c0147dec677a0b
yuling <gb2013429@gmail.com>
2025-03-17 23:45:54 +0800

    feat:RWD-header + homepage

--- src/views/frontend/HomeView.vue
+++ src/views/frontend/HomeView.vue
@@ -2,42 +2,44 @@
   <div class="flex w-full h-full">
     <!-- 左側固定區塊 -->
     <div
-      class="w-70 h-[calc(100vh-60px)] bg-[#d0d6dd26] flex flex-col fixed left-0 top-[60px] bottom-3 inset-y-3 m-3"
+      class="sm:w-20 md:w-70 h-[calc(100vh-56px)] bg-[#d0d6dd26] flex flex-col fixed left-0 top-[56px] bottom-3 inset-y-3 m-3 hidden sm:flex"
       v-if="menuVisible"
     >
       <!-- 內部內容，超出時才滾動 -->
-      <div class="p-3 flex-1 overflow-y-auto">
+      <div class="p-3 flex-1 overflow-y-auto hidden sm:block">
         <div class="card flex justify-center mb-5">
           <PanelMenu :model="menuList" class="w-full md:w-70">
             <template #item="{ item }">
               <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
                 <a
                   v-ripple
-                  class="no-border flex items-center cursor-pointer text-surface-700 dark:text-surface-0 px-4 py-2"
+                  class="no-border flex items-center justify-center cursor-pointer text-surface-700 dark:text-surface-0 px-4 py-2 rounded-sm"
                   :href="href"
+                  :class="{ 'bg-[#EF6C00] text-white': route.path === item.route }"
                   @click="navigate"
                 >
                   <span :class="item.icon" />
-                  <span class="ml-2">{{ item.label }}</span>
+                  <span class="ml-2 hidden md:block">{{ item.label }}</span>
                 </a>
               </router-link>
               <a
                 v-else
                 v-ripple
-                class="flex items-center cursor-pointer text-surface-700 dark:text-surface-0 px-4 py-2"
+                class="flex items-center justify-center cursor-pointer text-surface-700 dark:text-surface-0 px-4 py-2 rounded-sm"
+                :class="{ 'bg-[#EF6C00] text-white': route.path === item.route }"
                 :href="item.url"
                 :target="item.target"
               >
                 <span :class="item.icon" />
-                <span class="ml-2">{{ item.label }}</span>
+                <span class="ml-2 hidden md:block">{{ item.label }}</span>
                 <span v-if="item.items" class="pi pi-angle-down text-primary ml-auto" />
               </a>
             </template>
           </PanelMenu>
         </div>
-        <hr class="mb-4 mx-4 border-t border-0 border-surface-200 dark:border-surface-700" />
-        <span class="px-4 font-black">PAGES YOU LIKE</span>
-        <ul class="p-4 unstyled">
+        <hr class="mb-4 mx-4 border-t border-0 border-surface-200 dark:border-surface-700 hidden md:block" />
+        <span class="px-4 font-black hidden md:block">PAGES YOU LIKE</span>
+        <ul class="p-4 unstyled hidden md:block">
           <li v-for="(item, index) in pageLikeList" :key="index" class="my-3">
             <div class="flex items-center">
               <router-link :to="item.link" class="flex items-center">
@@ -55,9 +57,15 @@
     </div>
 
     <!-- 右側區塊 (可滾動) -->
-    <div class="flex-1 overflow-y-auto p-3" :class="{ 'ml-72': menuVisible, 'ml-0': menuVisible }">
+    <div class="flex-1 overflow-y-auto mt-1" :class="{ 'sm:ml-20 md:ml-75': menuVisible, 'ml-0': menuVisible }">
       <div class="bg-[#d0d6dd26] p-3 inset-y-3">
-        <h3 class="text-black mb-3 text-xl">Upcoming Friends</h3>
+        <div class="flex items-center justify-between mb-3">
+          <h3 class="text-black text-xl">Upcoming Friends</h3>
+          <div>
+            <Button icon="pi pi-microsoft" severity="secondary" @click="toggle" class="mx-1" />
+            <Button icon="pi pi-list" severity="secondary" @click="toggle" class="mx-1" />
+          </div>
+        </div>
         <Panel class="p-5 mb-5">
           <template #header>
             <div class="flex items-center gap-2">
@@ -74,8 +82,9 @@
           <template #footer>
             <div class="flex flex-wrap items-center justify-between gap-4">
               <div class="flex items-center gap-2">
-                <Button icon="pi pi-user" rounded text></Button>
-                <Button icon="pi pi-bookmark" severity="secondary" rounded text></Button>
+                <Button icon="pi pi-heart" rounded text></Button>
+                <Button icon="pi pi-comment" severity="secondary" rounded text></Button>
+                <Button icon="pi pi-send" severity="secondary" rounded text></Button>
               </div>
               <span class="text-surface-500 dark:text-surface-400">Updated 2 hours ago</span>
             </div>
@@ -110,13 +119,14 @@
             </div>
           </div>
 
-          <p class="">
+          <p class="text-justify">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
             exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
             dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
             mollit anim id est laborum.
+            <Button severity="secondary" label="more" @click="toggle" />
           </p>
         </Panel>
         <Panel class="p-5 mb-5">
@@ -135,8 +145,9 @@
           <template #footer>
             <div class="flex flex-wrap items-center justify-between gap-4">
               <div class="flex items-center gap-2">
-                <Button icon="pi pi-user" rounded text></Button>
-                <Button icon="pi pi-bookmark" severity="secondary" rounded text></Button>
+                <Button icon="pi pi-heart" rounded text></Button>
+                <Button icon="pi pi-comment" severity="secondary" rounded text></Button>
+                <Button icon="pi pi-send" severity="secondary" rounded text></Button>
               </div>
               <span class="text-surface-500 dark:text-surface-400">Updated 2 hours ago</span>
             </div>
@@ -171,13 +182,14 @@
             </div>
           </div>
 
-          <p class="">
+          <p class="text-justify">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
             exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
             dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
             mollit anim id est laborum.
+            <Button severity="secondary" label="more" @click="toggle" />
           </p>
         </Panel>
       </div>
@@ -186,14 +198,15 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
-
 import loginSocial from "@/assets/images/login-social-img.jpg";
 import springFirst from "@/assets/images/spring_first.jpg";
 import springSecond from "@/assets/images/spring_second.jpg";
-import { useRouter } from "vue-router";
 import { storeToRefs } from "pinia";
+import { useRouter, useRoute } from "vue-router";
+import { ref } from "vue";
+
 const router = useRouter();
+const route = useRoute();
 import { useHomeStore } from "@/stores/useHomeStore";
 
 const homeStore = useHomeStore();
@@ -203,6 +216,7 @@ const menuList = ref([
   {
     label: "home",
     icon: "pi pi-home",
+    route: "/",
     command: () => {
       router.push("/");
     },
@@ -289,5 +303,8 @@ const toggle = (event) => {
   position: absolute;
   top: 0;
   left: 0;
+}
+.text-bg {
+  background: #000;
 }
 </style>
